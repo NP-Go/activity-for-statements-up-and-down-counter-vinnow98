d0caf7007bfaa46a7224737dddedd306dc02308c
vinnow98 <evinwoon@gmail.com>
2022-04-26 20:04:38 +0800

    completed

--- upDownCounter.go
+++ upDownCounter.go
@@ -1,5 +1,25 @@
 package main
 
+import "fmt"
+
 func main() {
-	//Insert code here
+	var first int
+	var second int
+	//var tempVal int
+	fmt.Print("Enter first ")
+	fmt.Scanln(&first)
+	fmt.Print("Enter second ")
+	fmt.Scanln(&second)
+
+	if second < first {
+		tempVal := second
+		second = first
+		first = tempVal
+	}
+	for i := first; i < second; i++ {
+		fmt.Println(i)
+	}
+	for i := second; i >= first; i-- {
+		fmt.Println(i)
+	}
 }
